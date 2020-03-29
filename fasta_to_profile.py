d59792a63ae1275e8f9a14f68155eae863dccf23
saulpierotti <saulpierotti@gmail.com>
2020-03-29 12:58:40 +0200

    .

--- fasta_to_profile.py
+++ fasta_to_profile.py
@@ -17,7 +17,7 @@ def get_aln(aln_file):
     return D
 
 
-def get_profile(D_aln, all_aa="ARNDCQEGHILKMFPSTWYV"):
+def get_profile(D_aln, all_aa):
     n = len(list(D_aln.values())[0])
     M_aln = []
     for i in range(n):
@@ -39,8 +39,60 @@ def get_profile(D_aln, all_aa="ARNDCQEGHILKMFPSTWYV"):
     return profile
 
 
+def get_entropy(profile):
+    entropy = 0
+    for column in profile:
+        col_entropy = 0
+        for residue in column[:20]:  # I am avoiding to count the gap
+            if residue == 0:
+                continue
+            res_entropy = -residue * np.log(residue)
+            col_entropy += res_entropy
+        entropy += col_entropy
+    return entropy
+
+
+def get_logo(profile):
+    consensus = []
+    cons_entropy = []
+    cons_frequency = []
+    for column in profile:
+        residue = column[:20].max()
+        residue_index = column[:20].argmax()
+        res_entropy = str(
+            abs(residue * np.log(residue))
+        )  # abs() for avoiding -0.0 in conserved positions
+        res_entropy += "0" * (19 - len(res_entropy))  # just for padding in print
+        residue = str(residue) + "0" * (
+            5 - len(str(residue))
+        )  # just for padding in print
+        consensus.append(residue_index)
+        cons_frequency.append(residue)
+        cons_entropy.append(res_entropy)
+    return consensus, cons_frequency, cons_entropy
+
+
 if __name__ == "__main__":
+    aas_with_gap = "ARNDCQEGHILKMFPSTWYV-"
     aln_file = sys.argv[1]
     D_aln = get_aln(aln_file)
-    profile = get_profile(D_aln)
-    print(profile)
+    profile = get_profile(D_aln, aas_with_gap)
+    aln_entropy = get_entropy(profile)
+    logo = get_logo(profile)
+    zipped_logo = zip(range(1, len(logo[0]) + 1), logo[0], logo[1], logo[2])
+    print("Alignment entropy (S):", aln_entropy, "\n")
+    for my_tuple in zipped_logo:
+        pos = my_tuple[0]
+        res = aas_with_gap[:20][my_tuple[1]]
+        freq = my_tuple[2]
+        entropy = my_tuple[3]
+        print(
+            "Position:",
+            pos,
+            "\tResidue:",
+            res,
+            "\tFrequency:",
+            freq,
+            "\tEntropy:",
+            entropy,
+        )
