d716664575eeb5e22fd64a08a672a93ace921d24
saulpierotti <saulpierotti@gmail.com>
2020-04-18 10:00:10 +0200

    .

---
+++ blast_tab_simplify.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Author: Saul Pierotti
+# Mail: saulpierotti.bioinfo@gmail.com
+# Last updated: 17/04/2020
+#
+# Takes a BLAST output file in tabular format as first argument and prints
+# to STDOUT a simplified version of it.
+#
+# The output is a simplified version containing only the query UniProt ID
+# and the associated E-value.
+# 
+# It is possible to give an optional second argument that will be appended
+# at the end of each line. This is useful for instance for differentiating
+# classes for machine learning methods.
+
+blastfile="$1"
+class=$2
+
+if [ -f "$blastfile" ]; then
+	awk -v class="$class" '{split($1,v,"|"); print v[2],$11, class}' $blastfile
+else
+	echo "No input file given. This script requires a BLAST output file in tabular format as first argument."
+fi
