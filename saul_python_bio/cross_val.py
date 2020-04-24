e4803689eaf5109b769fe103bab15a1f8d9ffb1f
saulpierotti <saulpierotti@gmail.com>
2020-04-24 18:25:29 +0200

    .

--- saul_python_bio/cross_val.py
+++ saul_python_bio/cross_val.py
@@ -7,7 +7,7 @@ Last updated: 24/04/2020
 
 Takes a dataset and an optional tolerance as arguments
 Produces a report file containing the confusion matrix, ACC, MCC and selected
-treshold for 10 randomised cross validation runs on a 80/20 split of the
+threshold for 10 randomised cross validation runs on a 80/20 split of the
 dataset.
 It produces also a roc curve plot on the entire dataset and a plot of the MCC vs
 threshold used for all the training sets.
@@ -19,6 +19,7 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn import utils
+from sklearn import model_selection
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -32,9 +33,10 @@ def get_df(path):
     return df
 
 
-def get_train_test(df, test_prop=0.2):
-    test_size = int(test_prop * len(df))
-    df_test, df_train = np.split(df, [test_size])
+def get_train_test(df, set_indeces_iterator, numsplits=5):
+    set_indeces = next(set_indeces_iterator)
+    df_train = df.iloc[set_indeces[0]]
+    df_test = df.iloc[set_indeces[1]]
     return df_train, df_test
 
 
@@ -43,15 +45,15 @@ def __get_confusion_mat(df, thr):
     data_class = df["Class"].values
     true_pos, false_pos, true_neg, false_neg = 0, 0, 0, 0
     for i in range(len(feature)):
-        if feature[i] > thr and data_class[i] == 1:
+        if feature[i] < thr and data_class[i] == 1:
             true_pos += 1
-        elif feature[i] > thr and data_class[i] == 0:
+        elif feature[i] < thr and data_class[i] == 0:
             false_pos += 1
-        elif feature[i] <= thr and data_class[i] == 1:
+        elif feature[i] >= thr and data_class[i] == 1:
             false_neg += 1
-        elif feature[i] <= thr and data_class[i] == 0:
+        elif feature[i] >= thr and data_class[i] == 0:
             true_neg += 1
-    confusion_mat = ((true_pos, false_pos), (true_neg, false_neg))
+    confusion_mat = ((true_pos, false_pos), (false_neg, true_neg))
     return confusion_mat
 
 
@@ -65,9 +67,9 @@ def get_confusion_mat_skl(df, thr):
 def __get_ACC(df, thr):
     confusion_mat = __get_confusion_mat(df, thr)
     t_pos = confusion_mat[0][0]
-    t_neg = confusion_mat[1][0]
+    t_neg = confusion_mat[1][1]
     f_pos = confusion_mat[0][1]
-    f_neg = confusion_mat[1][1]
+    f_neg = confusion_mat[1][0]
     above_frac = t_pos + t_neg
     below_frac = t_pos + t_neg + f_pos + f_neg
     ACC = above_frac / below_frac
@@ -84,9 +86,9 @@ def get_ACC_skl(df, thr):
 def __get_MCC(df, thr):
     confusion_mat = __get_confusion_mat(df, thr)
     t_pos = confusion_mat[0][0]
-    t_neg = confusion_mat[1][0]
+    t_neg = confusion_mat[1][1]
     f_pos = confusion_mat[0][1]
-    f_neg = confusion_mat[1][1]
+    f_neg = confusion_mat[1][0]
     above_frac = t_pos * t_neg - f_pos * f_neg
     below_frac = np.sqrt(
         (t_pos + f_pos) * (t_pos + f_neg) * (t_neg + f_pos) * (t_neg + f_neg)
@@ -109,11 +111,10 @@ def get_stats(df, thr):
     return confusion_mat, ACC, MCC
 
 
-def thr_explore(df):
-    start = 20
-    stop = 30
+def thr_explore(df, start=-100, stop=1):
     MCC_list, thr_list = [], []
-    for thr in range(start, stop):
+    for exp in range(start, stop):
+        thr = 10 ** exp
         MCC = __get_MCC(df, thr)
         MCC_list.append(MCC)
         thr_list.append(thr)
@@ -121,8 +122,8 @@ def thr_explore(df):
     return MCC_list, thr_list
 
 
-def get_best_thr(df):
-    MCC, thr = thr_explore(df)
+def get_best_thr(df, start=-100, stop=1):
+    MCC, thr = thr_explore(df, start, stop)
     best_MCC = -2
     for i in range(len(MCC)):
         if MCC[i] > best_MCC:
@@ -130,24 +131,40 @@ def get_best_thr(df):
             best_MCC = MCC[i]
         elif MCC[i] == best_MCC:
             best_thr.append(thr[i])
-    thr_out = sum(best_thr) / len(best_thr)
+    thr_out = 10 ** (sum(np.log10(best_thr)) / len(best_thr))
     print("Selected threshold:", thr_out)
     return thr_out, thr, MCC
 
 
-def train_and_test(df, test_prop=0.2, num_replicates=10):
-    train_report, all_MCCs = [], []
+def train_and_test(df, numsplits=5, num_replicates=1, start=-100, stop=1):
+    train_report, all_MCCs, auc_list, tpr_list, fpr_list = [], [], [], [], []
     for i in range(num_replicates):
-        print("Training replicate", i + 1)
-        df = utils.shuffle(df)
-        df_train, df_cv = get_train_test(df, test_prop)
-        thr, thr_list, MCC_list = get_best_thr(df_train)
-        test_results = get_stats(df_cv, thr)
-        print("Performance on the test set", list(test_results))
-        train_report.append([test_results, thr])
-        all_MCCs += MCC_list
-    thr_MCC_report = [thr_list * num_replicates, all_MCCs]
-    return train_report, thr_MCC_report
+        print("\nTraining replicate", i + 1)
+        set_indeces_iterator = model_selection.StratifiedKFold(
+            n_splits=numsplits, shuffle=True, random_state=1
+        ).split(df, y=df["Class"])
+        for j in range(numsplits):
+            print("\nCross validation fold", j + 1)
+            df_train, df_cv = get_train_test(df, set_indeces_iterator, numsplits)
+            thr, thr_list, MCC_list = get_best_thr(df_train, start, stop)
+            confusion_mat, ACC, MCC = get_stats(df_cv, thr)
+            train_report.append((confusion_mat, ACC, MCC, thr))
+            print("\nPerformance on the test set:")
+            print("ACC", str(ACC))
+            print("MCC", str(MCC))
+            print("Confusion matrix", confusion_mat)
+            all_MCCs += MCC_list
+            y_true = df_cv["Class"].values
+            y_score = [-value for value in df_cv["Feature"].values]
+            fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
+            curr_auc = metrics.auc(fpr, tpr)
+            print("AUC of the test set", curr_auc)
+            auc_list.append(curr_auc)
+            tpr_list += list(tpr)
+            fpr_list += list(fpr)
+    thr_MCC_report = (thr_list * num_replicates * numsplits, all_MCCs)
+    roc_curve_report = (auc_list, tpr_list, fpr_list)
+    return train_report, thr_MCC_report, roc_curve_report
 
 
 def write_report(train_report_list):
@@ -155,27 +172,26 @@ def write_report(train_report_list):
     argv_index = 1
     with open("cross_val_report.dat", "w") as f:
         for train_report in train_report_list:
-            f.write("Input file: " + str(sys.argv[argv_index]) + "\n")
+            f.write("# Input file: " + str(sys.argv[argv_index]) + "\n")
             argv_index += 1
             f.write(
                 "tp"
                 + sep
                 + "fp"
                 + sep
-                + "tn"
-                + sep
                 + "fn"
+                + sep
+                + "tn"
                 + sep
                 + "ACC"
                 + sep
                 + "MCC"
                 + sep
                 + "thr"
-                + "\n---\n"
+                + "\n"
             )
             for train in train_report:
-                stats, thr = train[0], train[1]
-                cm, ACC, MCC = stats[0], stats[1], stats[2]
+                cm, ACC, MCC, thr = train[0], train[1], train[2], train[3]
                 f.write(
                     str(cm[0][0])
                     + sep
@@ -192,59 +208,93 @@ def write_report(train_report_list):
                     + str(thr)
                     + "\n"
                 )
-            list_MCC = [train[0][2] for train in train_report]
-            avg_MCC = sum(list_MCC) / len(train_report)
-            std_MCC = np.std(np.array(list_MCC))
-            avg_bitscore = sum([train[1] for train in train_report]) / len(train_report)
-            list_cm = [np.array(train[0][0]) for train in train_report]
-            avg_cm = [int(val) for row in np.mean(list_cm, axis=0) for val in row]
+            list_MCC = [train[2] for train in train_report]
+            arr_MCC = np.array(list_MCC)
+            avg_MCC = np.mean(arr_MCC)
+            std_MCC = np.std(arr_MCC)
+            avg_evalue = sum([np.log10(train[3]) for train in train_report]) / len(
+                train_report
+            )
+            list_cm = [np.array(train[0]) for train in train_report]
+            tp_arr = np.array([cm[0][0] for cm in list_cm])
+            fp_arr = np.array([cm[0][1] for cm in list_cm])
+            fn_arr = np.array([cm[1][0] for cm in list_cm])
+            tn_arr = np.array([cm[1][1] for cm in list_cm])
+            avg_tp = np.mean(tp_arr)
+            avg_fp = np.mean(fp_arr)
+            avg_fn = np.mean(fn_arr)
+            avg_tn = np.mean(tn_arr)
+            avg_cm = (avg_tp, avg_fp, avg_fn, avg_tn)
             f.write(
                 "---"
                 + "\nAverage MCC: "
                 + str(avg_MCC)
                 + "\nStandard deviation MCC: "
                 + str(std_MCC)
-                + "\nAverage bit score threshold: "
-                + str(avg_bitscore)
+                + "\nAverage E value threshold: "
+                + str(avg_evalue)
                 + "\nAverage confusion matrix: "
                 + str(avg_cm)
             )
-            f.write("\n### end of dataset\n")
+            if argv_index <= (len(sys.argv) - 1):
+                f.write("\n\n")
+            else:
+                f.write("\n")
 
 
-def plot_roc_curve(datasets):
-    for dataset in datasets:
-        y_true = dataset["Class"].values
-        y_score = dataset["Feature"].values
-        fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
-        roc_plt = sns.lineplot(x=fpr, y=tpr, markers=True)
-    plt.legend(labels=sys.argv[1:])
+def plot_roc_curve(roc_curve_report_list):
+    i = 1
+    for roc_curve_report in roc_curve_report_list:
+        auc_list, tpr_list, fpr_list = roc_curve_report
+        auc_arr = np.array(auc_list)
+        avg_auc = np.mean(auc_arr)
+        auc_sd = np.std(auc_arr)
+        sns.lineplot(
+            x=fpr_list,
+            y=tpr_list,
+            markers=True,
+            label=sys.argv[i]
+            + "\nMean AUC="
+            + str(round(avg_auc, 2))
+            + ", SD="
+            + "{:.2E}".format(auc_sd),
+        )
+        i += 1
     plt.ylabel("True Positive Rate")
     plt.xlabel("False Positive Rate")
     plt.savefig("roc_plot.png")
     plt.clf()
-    print("AUC for the ROC curve in the complete dataset:", metrics.auc(fpr, tpr))
 
 
 def plot_thr_MCC(thr_MCC_report_list):
     for thr_MCC_report in thr_MCC_report_list:
         sns.lineplot(x=thr_MCC_report[0], y=thr_MCC_report[1], markers=True)
+    plt.semilogx()
     plt.ylabel("MCC")
-    plt.xlabel("bit score threshold")
+    plt.xlabel("Threshold (E value)")
     plt.legend(labels=sys.argv[1:])
-    plt.savefig("bitscore_MCC_plot.png")
+    plt.savefig("evalue_MCC_plot.png")
     plt.clf()
 
 
-if __name__ == "__main__":
-    train_report_list, thr_MCC_report_list = [], []
-    dfs = []
+def main():
+    num_replicates = 1
+    numsplits = 5
+    start_thr = -30
+    stop_thr = 1
+    dfs, train_report_list, thr_MCC_report_list, roc_curve_report_list = [], [], [], []
     for path in sys.argv[1:]:
         df = get_df(path)
-        train_report, thr_MCC_report = train_and_test(df, 0.2, 1)
+        train_report, thr_MCC_report, roc_curve_report = train_and_test(
+            df, numsplits, num_replicates, start_thr, stop_thr
+        )
         train_report_list.append(train_report)
         thr_MCC_report_list.append(thr_MCC_report)
-        dfs.append(df)
-    plot_roc_curve(dfs)
+        roc_curve_report_list.append(roc_curve_report)
+    plot_roc_curve(roc_curve_report_list)
     plot_thr_MCC(thr_MCC_report_list)
     write_report(train_report_list)
+
+
+if __name__ == "__main__":
+    main()
--- shell_bioscripts/hmmalign_to_dataset.sh
+++ shell_bioscripts/hmmalign_to_dataset.sh
@@ -7,29 +7,24 @@
 # Takes a hmmsearch output file in tablular format as first argument and prints
 # to STDOUT a simplified version of it.
 # The hmmsearch output seemsto be uniquem but just for safety this script also
-# keeps only the highest bitscore line for each entry.
+# keeps only the lowest E-value line for each entry.
 #
 # The output is a simplified version containing only the query UniProt ID
-# and the associated bitscore.
+# and the associated E-value.
 # 
 # It is possible to give an optional second argument that will be appended
 # at the end of each line. This is useful for instance for differentiating
 # classes for machine learning methods.
 # By default it outputs the global E-value in the second field.
-# If I want the local corealue put 1 as fourth argument.
+# If I want the local E-value put 1 as fourth argument.
 
 hmmfile="$1"
 class="$2"
 is_local="$3"
 
 if [ -f "$hmmfile" ]; then
-	cat $hmmfile|grep -v "^#"|awk -v class=$class -v is_local=$is_local '{bitscore_loc=$9;bitscore_glob=$6;split($1,v,"|");\
-		if (is_local)\
-			{bitscore=bitscore_loc}\
-		else\
-			{bitscore=bitscore_glob};\
-		print v[2],bitscore,class}'\
-	|awk '{if (!($1 in out) ||$2 < bitscore[$1]) {out[$1]=$0; bitscore[$1]=$2}} END{for(key in out){print out[key]}}'|sort -k 1
+	cat $hmmfile|grep -v "^#"|awk -v class=$class -v is_local=$is_local '{eval_loc=$8;eval_glob=$5;split($1,v,"|"); if (is_local){eval=eval_loc}else{eval=eval_glob};print v[2],eval,class}'\
+	|awk '{if (!($1 in out) ||$2 < evalue[$1]) {out[$1]=$0; evalue[$1]=$2}} END{for(key in out){print out[key]}}'|sort -k 1
 
 else
 	echo "No input file given. This script requires an hmmsearch output file in tabular format as first argument."
